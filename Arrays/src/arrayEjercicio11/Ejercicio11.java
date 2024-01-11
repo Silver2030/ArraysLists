7860ff41994e47b5e0f32787ae3f6ce701018724
INFORMATICA\ivanrq <ivanrq@educastur.com>
2024-01-11 12:55:35 +0100

    Array

--- Arrays/src/arrayEjercicio11/Ejercicio11.java
+++ Arrays/src/arrayEjercicio11/Ejercicio11.java
@@ -10,50 +10,62 @@ public class Ejercicio11 {
 		int [] num = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28};
 		char [] carac = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 
 				'V', 'W', 'X', 'Y', 'Z', ' ', '.', ','};
-		int res = 0;
-		char respu = 'A';
-		String codigo = "S";
-		ArrayList<String> mensaje = new ArrayList<>();
+		int respuInt = 0;
+		char respuChar = 'A';
+		String mensaje = "A";
+		ArrayList<String> mensajeLetra = new ArrayList<>();
+		ArrayList<Integer> mensajeNum = new ArrayList<>();
 		
 		System.out.println("A: Descifrar Digitos");
 		System.out.print("B: Descifrar Letras \n Que tipo de mensaje desea descifrar: ");
-		respu = Character.toUpperCase(in.next().charAt(0));
-		while ((respu != 'A')&&(respu != 'B')){
+		respuChar = Character.toUpperCase(in.next().charAt(0));
+		while ((respuChar != 'A')&&(respuChar != 'B')){
 			System.out.println("A: Descifrar Digitos");
 			System.out.print("B: Descifrar Letras \n Respuesta no valida, que tipo de mensaje desea descifrar: ");
-			respu = Character.toUpperCase(in.next().charAt(0));
+			respuChar = Character.toUpperCase(in.next().charAt(0));
 		}
 		
-		if (respu == 'A') {
-			while (res != -1) {
+		if (respuChar == 'A') {
+			while (respuInt != -1) {
 				System.out.print("Introduce los digitos del codigo (-1 para finalizar): ");
-				res = in.nextInt();
-				while (((res<0)|(res>28))&&(res != -1)) {
+				respuInt = in.nextInt();
+				while (((respuInt<0)|(respuInt>28))&&(respuInt != -1)) { /* Validar que no acepte valores no catalogados */
 					System.out.print("Valor erroneo, introduce los digitos del codigo (-1 para finalizar): ");
-					res = in.nextInt();
+					respuInt = in.nextInt();
 				}
-				if(res != -1) mensaje.add(String.valueOf(codigoDigito(carac, num, res)));
+				if(respuInt != -1) mensajeLetra.add(String.valueOf(codigoDigito(carac, num, respuInt))); 
+				/* Llamar al metodo que cambio el int por un char y añadir al array dinamico */
 			}
 		}else {
-			
-		}
+				in.nextLine();
+				System.out.print("Introduce el mensaje a descifrar: ");
+				mensaje = in.nextLine().toUpperCase();
+				for (int i = 0; i < mensaje.length() ; i++) {
+					while ((( (int)mensaje.charAt(i) <65)|((int)mensaje.charAt(i)>90))&&((int)mensaje.charAt(i) != 32)&&((int)mensaje.charAt(i) != 44)
+							&&((int)mensaje.charAt(i) != 46)&&((int)mensaje.charAt(i) != 42)) { /* Validar que no acepte valores no catalogados */
+						System.out.print("Valor erroneo, introduce el mensaje a descifrar: ");
+						mensaje = in.nextLine().toUpperCase();
+				}
+					if(respuChar != '*') mensajeNum.add(codigoLetra(carac, num, mensaje.charAt(i))); 
+				} /* Llamar al metodo que cambio el char por una int y añadir al array dinamico */
+			}
 		
-		
-		if(mensaje.size() > 0) System.out.println(mensaje);
+		if(mensajeLetra.size() > 0) System.out.println(mensajeLetra);
+		if(mensajeNum.size() > 0) System.out.println(mensajeNum);
 
 	}
-	public static char codigoDigito(char [] carac, int [] num, int res) {
-		char resp = ' ';
+	public static char codigoDigito(char [] carac, int [] num, int respuInt) {
+		char resp = ' '; /* Metodo que identifica el char correpondiente al int dado */
 		for(int i = 0; i < num.length; i++) {
-			if (num[i] == res) resp = carac[i];
+			if (num[i] == respuInt) resp = carac[i];
 		}
 		return resp;
 	}
 	
-	public static int codigoLetra(char [] carac, int [] num, String respuesta) {
-		int resp = 0;
-		for(int i = 0; i < num.length; i++) {
-			if (num[i] == resp) resp = carac[i];
+	public static int codigoLetra(char [] carac, int [] num, char respuChar) {
+		int resp = 0; /* Metodo que identifica el int correpondiente al char dado */
+		for(int i = 0; i < carac.length; i++) {
+			if (carac[i] == respuChar) resp = num[i];
 		}
 		return resp;
 	}
